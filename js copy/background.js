6243b3cdacdcc390c1751ff164e822446a091745
maudev1 <mauriciojr.dev@gmai.com>
2022-01-25 23:38:23 -0300

    integraçao popup com background

---
+++ js copy/background.js
@@ -0,0 +1,115 @@
+let coins = [];
+
+chrome.extension.onConnect.addListener(function (port) {
+    // console.log(port);
+    port.onMessage.addListener(function (coin) {
+
+        
+
+        if(coin == 1){
+
+            chrome.storage.local.get(['coin'], function (result) {
+                console.log('Value currently is ' + result.coin);
+        
+                port.postMessage(result.coin);
+        
+            })
+
+        }else{
+
+            chrome.storage.local.set({ coin: coins }, function () {
+                console.log('Value is set to ' + coins);
+            });
+    
+
+        }
+
+     
+
+    // chrome.storage.local.get(['coin'], function (result) {
+    //     console.log('Value currently is ' + result.coin);
+
+    //     port.postMessage(result.coin);
+
+    // })
+
+       
+
+    
+
+
+    });
+
+    // chrome.storage.local.get(['coin'], function (result) {
+    //     console.log('Value currently is ' + result.coin);
+
+    //     port.postMessage(result.coin);
+
+        // if(result.key != 'maudev1'){
+
+        //     let value = 'maudev1';
+        //     console.log('teste')
+        //     chrome.storage.local.set({key: value}, function() {
+        //         console.log('Value is set to ' + value);
+        //       });
+
+
+        // }else{
+        //     console.log('Chave:' + result.key)
+        // }
+
+
+    // })
+})
+
+
+setInterval(() => {
+
+    getData('BTC');
+
+}, 2000)
+
+function getData(coin) {
+
+    let request = new XMLHttpRequest();
+
+    if (!request) {
+        console.log('Não foi possivel criar uma instancia XMLHTTP...');
+    }
+    request.onreadystatechange = messages;
+    request.open('GET', `https://www.mercadobitcoin.net/api/${coin}/ticker/`, true);
+    request.send()
+
+    function messages() {
+
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+
+                console.log(request.responseText);
+
+            } else {
+
+                console.log('erro ao trazer dados...')
+            }
+        }
+
+    }
+
+    function alert() {
+
+        chrome.notifications.create('test', {
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: 'Test Message',
+            message: 'You are awesome!',
+            priority: 2
+        });
+
+    }
+
+
+}
+
+
+
+
--- popups/js/script.js
+++ popups/js/script.js
@@ -1,15 +1,52 @@
 
+
+
+
+
 jQuery(($) => {
 
-    let Cryptocurrencies = ['BTC', 'ETH', 'DOGE'];
 
-    getData(Cryptocurrencies);
+    $('.add').on('click', function () {
+
+        let coin = $('.coins').val();
+
+        chrome.storage.local.set({ coins: coin }, function () {
+
+        });
+
+        chrome.storage.local.get(['coins'], (result) => {
+            //console.log(result.coins)
+            let Data = [];
+    
+            Data.push(result.coins);
+    
+            getData(Data);
+        });
+
+        coin = '';
+    })
+
+    chrome.storage.local.get(['coins'], (result) => {
+        //console.log(result.coins)
+        let Data = [];
+
+        Data.push(result.coins);
+
+        getData(Data);
+    });
 
     // auto-refresh 
 
     setInterval(() => {
-        
-        getData(Cryptocurrencies);
+
+        chrome.storage.local.get(['coins'], (result) => {
+            //console.log(result.coins)
+            let Data = [];
+    
+            Data.push(result.coins);
+    
+            getData(Data);
+        });
 
     }, 20000)
 
@@ -27,63 +64,63 @@ jQuery(($) => {
      * */
 
 
-        function getData(Cryptocurrencies) {
+    function getData(Cryptocurrencies) {
 
-            var quotes = '';
+        var quotes = '';
 
-            $('.body').html('');
-            $('.progress').removeClass('is-hidden');
+        $('.body').html('');
+        $('.progress').removeClass('is-hidden');
 
-            Cryptocurrencies.forEach(coin => {
+        Cryptocurrencies.forEach(coin => {
 
-                $.get(`https://www.mercadobitcoin.net/api/${coin}/ticker/`, {
+            $.get(`https://www.mercadobitcoin.net/api/${coin}/ticker/`, {
 
-                }).then((data) => {
+            }).then((data) => {
 
-                    let open = data.ticker.open;
-                    let current = data.ticker.last;
+                let open = data.ticker.open;
+                let current = data.ticker.last;
 
                 var formatter = new Intl.NumberFormat('pt-BR', {
                     style: 'currency',
                     currency: 'BRL',
-  
-                    });
-  
-                     quotes = $(`<tr>` +
-                        `<td><span class="tag is-warning"><b><a class="has-text-maudev" target="_BLANK" href="https://www.google.com.br/search?q=${coin}">${coin}</a></b></span></td>` +
-                        `<td>` +
-                        `<span class="price"> ${formatter.format(current)}</span>` +
-                        `<span class="arrow-up">` +
-                        `<i class="material-icons ${current < open ? 'has-text-success': 'has-text-danger'} ">${current < open ? 'arrow_drop_up': 'arrow_drop_down'}</i>` +
-                        `</span>` +
-                        `</td>` +
-                        `<td class="has-text-centered">` +
-                        `<span class="set-alarm">` +
-                        `<a title="definir alarme" ><i class="material-icons has-text-maudev">notifications_active</i></a>` +
-                        `</span>` +
-                        `</td>` +
-                        `</tr>`);
 
-                    $('.progress').addClass('is-hidden');
+                });
 
-                    quotes.appendTo('.body');
+                quotes = $(`<tr>` +
+                    `<td><span class="tag is-warning"><b><a class="has-text-maudev" target="_BLANK" href="https://www.google.com.br/search?q=${coin}">${coin}</a></b></span></td>` +
+                    `<td>` +
+                    `<span class="price"> ${formatter.format(current)}</span>` +
+                    `<span class="arrow-up">` +
+                    `<i class="material-icons ${current < open ? 'has-text-success' : 'has-text-danger'} ">${current < open ? 'arrow_drop_up' : 'arrow_drop_down'}</i>` +
+                    `</span>` +
+                    `</td>` +
+                    `<td class="has-text-centered">` +
+                    `<span class="set-alarm">` +
+                    `<a title="definir alarme" ><i class="material-icons has-text-maudev">notifications_active</i></a>` +
+                    `</span>` +
+                    `</td>` +
+                    `</tr>`);
 
+                $('.progress').addClass('is-hidden');
 
-                }).catch((error) => {
-
-                    $('.progress').removeClass('is-hidden');
-
-                    console.log(`Houve um erro, tente novamente, error: ${error}`)
-                })
-
-            });
-            
-            // $('.body').html(quotes);
-
-        }
+                quotes.appendTo('.body');
 
 
-    
+            }).catch((error) => {
+
+
+
+                console.log(`Houve um erro, tente novamente, error: ${error}`)
+            })
+
+        });
+
+        // $('.body').html(quotes);
+
+    }
+
+
+
 
 
 })
