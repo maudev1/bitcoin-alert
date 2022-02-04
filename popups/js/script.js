8e80cdbf8541e84c09eed53b89c118831643ddce
mauricio <mauricio@vgt.com.br>
2022-02-04 18:02:52 -0300

    mudanças no layout e calculo de valores

--- popups/js/script.js
+++ popups/js/script.js
@@ -12,11 +12,12 @@ jQuery(($) => {
     //     'placeholder': ''
     // });
 
-    $('.color').on('change',function(){
+
+    $('.color').on('change', function () {
 
         console.log($(this).val());
 
-        $('.is-maudev').css('background-color',`${$(this).val()}`);
+        $('.is-maudev').css('background-color', `${$(this).val()}`);
 
 
     })
@@ -28,7 +29,7 @@ jQuery(($) => {
     });
 
     let favorites = [];
-    
+
     chrome.storage.local.get(['coins'], (result) => {
 
         if (!result.coins) {
@@ -44,7 +45,7 @@ jQuery(($) => {
 
     });
 
-    $('.coins').on('change',()=>{
+    $('.coins').on('change', () => {
 
         returnVal($('.coins').val());
 
@@ -57,31 +58,29 @@ jQuery(($) => {
 
         console.log(price);
 
-        //throw 'teste';
-
         let coins = [coin, price];
 
         if (!favorites.includes(coins)) {
-            
+
             favorites.push(coins);
 
             chrome.storage.local.set({ coins: favorites }, function () { });
-            
+
             saveInLocal(favorites);
 
         }
 
         console.log(favorites);
-             
+
         getData(favorites);
     })
 
-    $('.reset-data').on('click',()=>{
-        
+    $('.reset-data').on('click', () => {
+
         favorites = [];
-       
+
         clearAllData('coins');
-        
+
         getData(favorites);
 
     })
@@ -93,7 +92,6 @@ jQuery(($) => {
         getData(favorites);
 
     }, 20000)
-
 
     /**
      * 
@@ -107,7 +105,6 @@ jQuery(($) => {
      * 
      * */
 
-
     function getData(values) {
 
         var quotes = '';
@@ -120,22 +117,56 @@ jQuery(($) => {
             $.get(`https://www.mercadobitcoin.net/api/${coin[0]}/ticker/`, {
 
             }).then((data) => {
-
+                //API DATA
                 let open = data.ticker.open;
                 let current = data.ticker.last;
 
-                quotes = $(`<tr>` +
-                    `<td><span class="tag is-warning"><b><a class="has-text-dark" target="_BLANK" href="https://www.google.com.br/search?q=${coin[0]}">${coin[0]}</a></b></span></td>` +
-                    `<td>`+
-                    `<span class="alarm"> ${formatter.format(coin[1])}</span>` +
-                    `</td>`+
+                //USER DATA
+                let currencyInitials = coin[0];
+                let priceAlert = coin[1];
+                let percent = null;
+
+                if (current > priceAlert) {
+                    let x = current - priceAlert;
+                    let y = x / current * 100;
+                    percent = y.toFixed(2) + ' %';
+
+                }
+                else {
+                    let x = current - priceAlert;
+                    let y = x / priceAlert * 100;
+                    percent = y.toFixed(2);
+
+
+                }
+
+
+                quotes = $(
+                    `<tr>
+                    <td>
+                    <div class="field is-grouped is-grouped-multiline">
+                        <div class="control">
+                            <div class="tags has-addons">
+                                <span class="tag is-dark"><i class="cf cf-${currencyInitials.toLowerCase()}"></i></span>
+                                <span class="tag is-warning">
+                                    <a class="has-text-dark" target="_BLANK" href="https://www.google.com.br/search?q=${coin[0]}">${coin[0]}</a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    </td>` +
                     `<td>` +
-                    `<span class="icon-text">`+
+                    `<span class="alarm"> ${formatter.format(priceAlert)}</span>` +
+                    `<span class="icon-text">` +
+                    `<span class="icon">` +
+                    `<i class="material-icons ${current < priceAlert ? 'has-text-success' : 'has-text-danger'}">${current < priceAlert ? 'arrow_drop_up' : 'arrow_drop_down'}</i>` +
+                    `</span>` +
+                    `<span style="font-size:10px" class="percent ${current < priceAlert ? 'has-text-success' : 'has-text-danger'}">${Math.abs(percent)}%</span>` +
+                    `</span>` +
+                    `</td>` +
+                    `<td>` +
                     `<span class="price"> ${formatter.format(current)}</span>` +
-                    `<span class="icon">`+
-                    `<i class="material-icons ${current < open ? 'has-text-success' : 'has-text-danger'} ">${current < open ? 'arrow_drop_up' : 'arrow_drop_down'}</i>` +
-                    `</span>`+
-                    `</span>`+
                     `</td>` +
                     `<td class="has-text-centered">` +
                     `<span class="set-alarm">` +
@@ -159,15 +190,15 @@ jQuery(($) => {
 
     }
 
-    function returnVal(value){
+    function returnVal(value) {
         $.get(`https://www.mercadobitcoin.net/api/${value}/ticker/`, {
 
-        }).then((data)=>{
+        }).then((data) => {
 
             $('#price').val('');
             $('#price').val(data.ticker.last);
 
-        }).catch((err)=>{
+        }).catch((err) => {
             console.log(err);
         })
 
